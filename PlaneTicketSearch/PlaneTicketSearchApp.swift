b45cb5f32519cc0a868bb0e80eaee8f2936548a6
StasiaKuzya <kuzina-sta@mail.ru>
2024-05-28 11:17:37 +0300

    Initial Commit

---
+++ PlaneTicketSearch/PlaneTicketSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaneTicketSearchApp.swift
+//  PlaneTicketSearch
+//
+//  Created by Анастасия on 28.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct PlaneTicketSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
